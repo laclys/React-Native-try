068c88755b5b05a5d3aa8d5016d31470d01694f4
Lac <laclys@126.com>
2017-07-31 21:32:28 +0800

    added

--- android/app/src/main/java/com/testnavigator/MainApplication.java
+++ android/app/src/main/java/com/testnavigator/MainApplication.java
@@ -3,6 +3,7 @@ package com.testnavigator;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.github.xfumihiro.react_native_image_to_base64.ImageToBase64Package;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
             new VectorIconsPackage(),
             new RNFetchBlobPackage(),
             new ImageToBase64Package()
